021fb585025546bb2819aa6f5cb3d5fbace0a79f
HeavyGuitarRiffs <truelee214@gmail.com>
2024-08-06 19:44:30 +0700

    Add files via upload
    This is the wireframe of HTML Reactjs and Vue

---
+++ App.js
@@ -0,0 +1,591 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo"/>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+
+        <body>
+
+        <div id="app"></div> 
+
+        <script
+  src="https://unpkg.com/vue@3/dist/vue.global.js">
+</script>
+
+<div class="align-center">
+				<br />
+                <strong>Bitcoin Donation</strong>
+                <br />
+                <small>1t8uXW6EE7HQ7</small>
+            </div>
+          
+        <h1>R3@|ity |S @ ~</h1>
+        <img src="w3schools.jpg" alt="W3Schools.com" width="1040" height="1040"></img>
+        <h2>Agile | Lean | Sprint</h2>
+        
+
+
+        <h2>Absolute URLs</h2>
+<p><a href="https://www.w3.org/">W3C</a></p>
+<p><a href="https://www.google.com/">Google</a></p>
+
+<h2>Relative URLs</h2>
+<p><a href="html_images.asp">HTML Images</a></p>
+<p><a href="/css/default.asp">CSS Tutorial</a></p>
+
+
+
+
+
+
+        <hr></hr>
+        <h3>Solidity Rust Reactjs PowerShell Javascript Typescript Java Go Pandas Python SQL PHP HTML CSS</h3>
+        <span>Phantom of Exponents Eating Power As A Bonus</span>
+        <p>Things To Be Saved: Money, Coding, and Your Games</p>
+        <h4>Heading 4</h4>
+
+
+
+        <p>A Quote From My Own Website Reads:</p>
+<blockquote cite="http://www.worldwildlife.org/who/index.html">
+Markets are in flux, times are always changing, and skills are always to be learned and refined.
+This? Well, this is a reminder of that.
+</blockquote>
+
+<p>My goal is to: <q>Build a future where people live in harmony with nature.</q></p>
+
+<p>The <abbr title="My Ideas Have Been">3000</abbr> in concept since 2014</p>
+
+<p><cite>Lorem</cite> by Dave Cho in 2002.</p>
+<bdo dir="rtl">Owing and Owning is Not the Same Thing. Deadpool is the Snowden of Hollywood.</bdo>
+
+
+<table>
+  <tr>
+    <th>Company</th>
+    <th>Contact</th>
+    <th>Country</th>
+  </tr>
+  <tr>
+    <td>VH</td>
+    <td>John Stamos</td>
+    <td>USA</td>
+  </tr>
+  <tr>
+    <td>Sun Fragment</td>
+    <td>Minh Hieu</td>
+    <td>Vietnam</td>
+  </tr>
+</table>
+
+
+<table>
+  <tr>
+    <td>Vue</td>
+    <td>C</td>
+    <td>C++</td>
+    <td>C#</td>
+    <td>Angular.js</td>
+  </tr>
+</table>
+
+<table>
+  <tr>
+    <td>Emil</td>
+    <td>Tobias</td>
+    <td>Linus</td>
+  </tr>
+  <tr>
+    <td>16</td>
+    <td>14</td>
+    <td>10</td>
+  </tr>
+</table>
+
+<table>
+  <tr>
+    <th>Person 1</th>
+    <th>Person 2</th>
+    <th>Person 3</th>
+  </tr>
+  <tr>
+    <td>Emil</td>
+    <td>Tobias</td>
+    <td>Linus</td>
+  </tr>
+  <tr>
+    <td>16</td>
+    <td>14</td>
+    <td>10</td>
+  </tr>
+</table>
+
+<table>
+  <tr>
+    <th>Firstname</th>
+    <td>Jill</td>
+    <td>Eve</td>
+  </tr>
+  <tr>
+    <th>Lastname</th>
+    <td>Smith</td>
+    <td>Jackson</td>
+  </tr>
+  <tr>
+    <th>Age</th>
+    <td>94</td>
+    <td>50</td>
+  </tr>
+</table>
+
+<table>
+  <tr>
+    <th>Firstname</th>
+    <th>Lastname</th>
+    <th>Age</th>
+  </tr>
+  <tr>
+    <td>Jill</td>
+    <td>Smith</td>
+    <td>50</td>
+  </tr>
+  <tr>
+    <td>Eve</td>
+    <td>Jackson</td>
+    <td>94</td>
+  </tr>
+</table>
+
+<table>
+  <tr>
+    <th>Name</th>
+    <td>Jill</td>
+  </tr>
+  <tr>
+    <th rowspan="2">Phone</th>
+    <td>555-1234</td>
+  </tr>
+  <tr>
+    <td>555-8745</td>
+</tr>
+</table>
+
+<table>
+  <tr>
+    <th colspan="2">Name</th>
+    <th>Age</th>
+  </tr>
+  <tr>
+    <td>Jill</td>
+    <td>Smith</td>
+    <td>43</td>
+  </tr>
+  <tr>
+    <td>Eve</td>
+    <td>Jackson</td>
+    <td>57</td>
+  </tr>
+</table>
+
+<ul>
+  <li>Coffee</li>
+  <li>Tea</li>
+  <li>Milk</li>
+</ul>
+
+<ol>
+  <li>Coffee</li>
+  <li>Tea</li>
+  <li>Milk</li>
+</ol>
+
+<dl>
+  <dt>Coffee</dt>
+  <dd>- black hot drink</dd>
+  <dt>Milk</dt>
+  <dd>- white cold drink</dd>
+</dl>
+
+<ol>
+  <li>Coffee</li>
+  <li>Tea
+    <ol>
+      <li>Black tea</li>
+      <li>Green tea</li>
+    </ol>
+  </li>
+  <li>Milk</li>
+</ol>
+
+<dl>
+  <dt>Coffee</dt>
+  <dd>- black hot drink</dd>
+  <dt>Milk</dt>
+  <dd>- white cold drink</dd>
+</dl>
+
+<p>Hello World</p>
+<div>Hello World</div>
+
+<div>
+  <h2>London</h2>
+  <p>London is the capital city of England.</p>
+  <p>London has over 13 million inhabitants.</p>
+</div>
+
+<div>
+  <h2>Oslo</h2>
+  <p>Oslo is the capital city of Norway.</p>
+  <p>Oslo has over 600.000 inhabitants.</p>
+</div>
+
+<div>
+  <h2>Rome</h2>
+  <p>Rome is the capital city of Italy.</p>
+  <p>Rome has almost 3 million inhabitants.</p>
+</div>
+
+<code>
+x = 5;
+y = 6;
+z = x + y;
+</code>
+
+<pre>
+<code>
+x = 5;
+y = 6;
+z = x + y;
+</code>
+</pre>
+
+<p>The area of a triangle is: 1/2 x <var>b</var> x <var>h</var>, where <var>b</var> is the base, and <var>h</var> is the vertical height.</p>
+
+<section>
+<h1>WWF</h1>
+<p>The World Wide Fund for Nature (WWF) is an international organization working on issues regarding the conservation, research and restoration of the environment, formerly named the World Wildlife Fund. WWF was founded in 1961.</p>
+</section>
+
+<section>
+<h1>WWF's Panda symbol</h1>
+<p>The Panda has become the symbol of WWF. The well-known panda logo of WWF originated from a panda named Chi Chi that was transferred from the Beijing Zoo to the London Zoo in the same year of the establishment of WWF.</p>
+</section>
+
+<article>
+<h2>Google Chrome</h2>
+<p>Google Chrome is a web browser developed by Google, released in 2008. Chrome is the world's most popular web browser today!</p>
+</article>
+
+<article>
+<h2>Mozilla Firefox</h2>
+<p>Mozilla Firefox is an open-source web browser developed by Mozilla. Firefox has been the second most popular web browser since January, 2018.</p>
+</article>
+
+<article>
+<h2>Microsoft Edge</h2>
+<p>Microsoft Edge is a web browser developed by Microsoft, released in 2015. Microsoft Edge replaced Internet Explorer.</p>
+</article>
+
+<article>
+  <header>
+    <h1>My Browser will be partnered to Vechain or another Crypto like Brave</h1>
+    <p>New Browser Mission</p>
+  </header>
+  <p>The Crypto Tab Browser had a great mining idea. Maybe there is room for growth?</p>
+</article>
+
+<p>The Crypto Space Will Partner to Many new and Exciting Companies</p>
+
+<aside>
+<h4>Distraction</h4>
+<p>Opposition means success is near. Count your growth by your rivals.</p>
+</aside>
+
+<section>
+  <p>I plan to make $100,000 my first year.</p>
+  <p>A $1,000,000 in 2 years.</p>
+</section>
+
+<table class="striped"></table>
+
+
+<h1>Famous Cities</h1>
+
+<h2>Tokyo</h2>
+<p>Tokyo is the capital of Japan, the center of the Greater Tokyo Area, and the most populous metropolitan area in the world.</p>
+
+<h2>London</h2>
+<p>London is the capital city of England. It is the most populous city in the United Kingdom.</p>
+
+<h2>Paris</h2>
+<p>Paris is the capital of France. The Paris area is one of the largest population centers in Europe.</p>
+
+<table>
+  <tr>
+    <th>Name</th>
+    <th>Description</th>
+  </tr>
+  <tr>
+    <td>A</td>
+    <td>Description of A</td>
+  </tr>
+  <tr>
+    <td>B</td>
+    <td>Description of B</td>
+  </tr>
+</table>
+
+<ul>
+  <li>Hong Kong</li>
+  <li>USA</li>
+  <li>Thailand</li>
+</ul>
+
+getElementById("Demo").innerHTML = "Hello";
+
+getElementById("demo").innerHTML = "Hello";
+
+<p>I will display &#8383;</p>
+<p>I will display &#8364;</p>
+<p>I will display &#x20AC;</p>
+
+<p>I will display A New Currency</p>
+<p>I will display &#65; &#66; &#67;</p>
+
+<h1>My First Of Many Renders</h1>
+
+<p>&#128512;</p>
+
+<b><i>Very Cool Builds Come From Ideas</i></b>
+
+<p>What sites can you visit daily?</p>
+<p>What problems around you can you solve?</p>
+
+<img src="happy.gif" alt="Happy face" />
+
+<body>
+<p>Make a product that solves a problem and sell it well</p>
+</body>
+
+<a href="https://www.w3schools.com/html/">Visit our HTML tutorial</a>
+
+<a href="https://www.w3schools.com/html/">Visit our HTML tutorial</a>
+
+<input type="checkbox" name="vehicle" value="car" checked="checked" />
+<input type="text" name="lastname" disabled="disabled" />
+
+<form>
+.
+form elements
+.
+</form>
+
+<form action="/action_page.php" target="_blank"></form>
+
+<form action="/action_page.php" method="get"></form>
+
+<form action="/action_page.php" method="post"></form>
+
+<form action="/action_page.php" autocomplete="on"></form>
+
+<form action="/action_page.php" novalidate></form>
+
+<label for="cars">Choose a car:</label>
+<select id="cars" name="cars">
+  <option value="volvo">Volvo</option>
+  <option value="saab">Saab</option>
+  <option value="fiat">Fiat</option>
+  <option value="audi">Audi</option>
+</select>
+
+<option value="fiat" selected>Fiat</option>
+
+<label for="cars">Choose a car:</label>
+<select id="cars" name="cars" size="3">
+  <option value="volvo">Volvo</option>
+  <option value="saab">Saab</option>
+  <option value="fiat">Fiat</option>
+  <option value="audi">Audi</option>
+</select>
+
+<textarea name="message" rows="10" cols="30">
+I'll have 30 SaaS Businesses in a Year
+</textarea>
+
+<button type="button" onclick="alert('Hello World!')">Click Me!</button>
+
+<input type="button" onclick="alert('Hello World!')" value="Click Me!">
+</input>
+
+<canvas id="myCanvas" width="200" height="100"></canvas>
+
+<script>
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+ctx.moveTo(0, 0);
+ctx.lineTo(200, 100);
+ctx.stroke();
+</script>
+
+<script>
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+ctx.beginPath();
+ctx.arc(95, 50, 40, 0, 2 * Math.PI);
+ctx.stroke();
+</script>
+
+<script>
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+ctx.font = "30px Arial";
+ctx.fillText("Hello World", 10, 50);
+</script>
+
+<script>
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+ctx.font = "30px Arial";
+ctx.strokeText("Hello World", 10, 50);
+</script>
+
+<script>
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+var img = document.getElementById("scream");
+ctx.drawImage(img, 10, 10);
+</script>
+
+<svg width="100" height="100">
+  <circle cx="50" cy="50" r="40" stroke="green" stroke-width="4" fill="yellow" />
+</svg>
+
+<svg width="400" height="120">
+  <rect x="10" y="10" width="200" height="100" stroke="red" stroke-width="6" fill="blue" />
+</svg>
+
+<svg height="130" width="500">
+  <defs>
+    <linearGradient id="grad1">
+      <stop offset="0%" stop-color="yellow" />
+      <stop offset="100%" stop-color="red" />
+    </linearGradient>
+  </defs>
+  <ellipse cx="100" cy="70" rx="85" ry="55" fill="url(#grad1)" />
+  <text fill="#ffffff" font-size="45" font-family="Verdana" x="50" y="86">SVG</text>
+  Sorry, your browser does not support inline SVG.
+</svg>
+
+if (typeof(Storage) !== "undefined") {
+  // Code for localStorage/sessionStorage.
+} else {
+  // Sorry! No Web Storage support..
+}
+
+if (typeof(Worker) !== "undefined") {
+  // Yes! Web worker support!
+  // Some code.....
+} else {
+  // Sorry! No Web Worker support..
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<iframe src="demo_iframe.htm" height="200" width="300" title="Iframe Example"></iframe>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<h5>Heading 5</h5>
+<b>This text is bold</b>
+<ins>This is a paragraph.</ins>  {/* Inserted text */} 
+    <del>This is a paragraph.</del> {/* Deleted text */} 
+    <small>This is a paragraph.</small>  {/* Smaller text */} 
+    <b>This text is bold</b> {/* bold text  */} 
+    <b>This text is bold</b>  {/* comment here */} 
+    <strong>This text is bold</strong>  {/* Important text */} 
+    <i>This text is bold</i>  {/* italic text */} 
+    <em>This text is bold</em>  {/* emphasized text */} 
+    <mark>This text is bold</mark>  {/* marked text */} 
+
+    <sub>This text is bold</sub>  {/* Subscript text */} 
+    <sup>This text is bold</sup>  {/* Superscript text */} 
+<h6>Heading 6</h6>
+        
+        <p>My first paragraph.</p>
+       <aside><a href="https://www.behance.net/justinbeau">Portfolio</a> </aside> 
+
+       <aside><a href="https://github.com/HeavyGuitarRiffs">GitHub</a></aside>
+
+        <div><a href="https://www.linkedin.com/in/justin-beau-033730171/">Linkedin</a></div>
+
+        <div><img src="img_girl.jpg" alt="Girl with a jacket"></img></div>
+
+
+        <button onclick="document.location='default.asp'">HTML Tutorial</button>
+
+
+
+
+        <a href="mailto:someone@example.com">Send email</a>
+
+
+
+
+        <footer>
+        <p>Justin Beau</p>
+      <p>All Rights Reseserved 2024 </p>
+    </footer>
+        
+        </body>
+      </header>
+
+      
+    </div>
+  );
+}
+
+export default App;
